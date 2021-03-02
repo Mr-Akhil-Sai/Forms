a754d4bd851905058ccf7232fb6248f149c4f5b5
Mr.AK <akhilsaivaraganti@gmail.com>
2021-03-02 14:24:10 +0530

    need to write the logic for edit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let submitButton = document.querySelector("#submit");
 let tabelDiv = document.querySelector("#tabelDiv");
+let tablerow = document.createElement("tr");
 
 submitButton.addEventListener("click", () => {
   let name = document.querySelector(".name");
@@ -7,15 +8,21 @@ submitButton.addEventListener("click", () => {
   let mail = document.querySelector(".mail");
 
   if (name.value != "" && phone.value != "" && mail.value != "") {
-    let table = document.querySelector("table");
-    let tablerow = document.createElement("tr");
-    tablerow.innerHTML = `
-	 <td>${name.value}</td> 
-	 <td>${phone.value}</td> 
-	 <td>${mail.value}</td> 
-	  `;
-    table.appendChild(tablerow);
-    clearInputs(name, phone, mail);
+    let email = mail.value;
+    if ((typeof phone.value == Number, email.includes("@"))) {
+      let table = document.querySelector("table");
+      tablerow.innerHTML = `
+			<td>${name.value}</td>
+			<td>${phone.value}</td>
+			<td>${mail.value}</td>
+			<td><button onclick= "editFunction()">edit</button></td>
+			<td><button onclick= "deleteFunction()" >delete</button></td>
+			`;
+      table.appendChild(tablerow);
+      clearInputs(name, phone, mail);
+    } else {
+      alert("enter the fields correctly");
+    }
   } else {
     alert("enter the given fileds");
   }
@@ -25,4 +32,10 @@ function clearInputs(name, phone, mail) {
   name.value = "";
   phone.value = "";
   mail.value = "";
+}
+
+function editFunction() {}
+
+function deleteFunction() {
+  tablerow.remove();
 }
