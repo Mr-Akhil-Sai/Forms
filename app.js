a700b5b3f0726f267f266144a7d709602f83807c
Mr.AK <akhilsaivaraganti@gmail.com>
2021-03-02 08:03:52 +0530

    refactored the code

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 let submitButton = document.querySelector("#submit");
 let tabelDiv = document.querySelector("#tabelDiv");
 
-function submit(){
-	let tabel = document.createElement("tabel")
-	tabel.innerText = `<tr>
+function submit() {
+  let tabel = document.createElement("tabel");
+  tabel.innerText = `<tr>
 				<th>Name</th>
 				<th>Phone-Number</th>
-				<th>Percentage</th>`
-	tabelDiv.appendChild(tabel);
+				<th>Percentage</th>`;
+  tabelDiv.appendChild(tabel);
 }
-
