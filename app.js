8b84ae411c92e09a7b6c7733d3c1ef077577528d
Mr.AK <akhilsaivaraganti@gmail.com>
2021-03-02 12:23:26 +0530

    need to work on edit and delete

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 let submitButton = document.querySelector("#submit");
 let tabelDiv = document.querySelector("#tabelDiv");
 
-function submit() {
-  let tabel = document.createElement("tabel");
-  tabel.innerText = `<tr>
-				<th>Name</th>
-				<th>Phone-Number</th>
-				<th>Percentage</th>`;
-  tabelDiv.appendChild(tabel);
+submitButton.addEventListener("click", () => {
+  let name = document.querySelector(".name");
+  let phone = document.querySelector(".phone");
+  let mail = document.querySelector(".mail");
+
+  if (name.value != "" && phone.value != "" && mail.value != "") {
+    let table = document.querySelector("table");
+    let tablerow = document.createElement("tr");
+    tablerow.innerHTML = `
+	 <td>${name.value}</td> 
+	 <td>${phone.value}</td> 
+	 <td>${mail.value}</td> 
+	  `;
+    table.appendChild(tablerow);
+    clearInputs(name, phone, mail);
+  } else {
+    alert("enter the given fileds");
+  }
+});
+
+function clearInputs(name, phone, mail) {
+  name.value = "";
+  phone.value = "";
+  mail.value = "";
 }
