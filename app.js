96b2ac1a11b1c41cb7d73f92ba6d7d2d5030827e
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-03-03 12:51:26 +0530

    implemented edit button

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ submitButton.addEventListener("click", () => {
     let email = mail.value;
     if ((typeof phone.value == Number, email.includes("@"))) {
       let tablerow = document.createElement("tr");
-      tablerow.innerHTML = `
-			<td>${name.value}</td>
-			<td>${phone.value}</td>
-			<td>${mail.value}</td>
+      tablerow.innerHTML =  `
+			<td id = "col1">${name.value}</td>
+			<td id = "col2">${phone.value}</td>
+			<td id = "col3">${mail.value}</td>
       <button onclick = "deleteFunction(this)">delete</button>
       <button onclick = "editFunction(this)">edit</button>
 			`;
@@ -49,10 +49,22 @@ function deleteFunction(tablerow) {
 function editFunction(tablecol) {
 	let row = tablecol.parentNode;
 	let values = row.innerText;
-	let array = [];
 	let value = values.split("\t");
 	name.value = value[0];
 	phone.value = value[1];
 	mail.value = value[2];
-	
+	submitButton.style.display = "none";
+	let addButton = document.querySelector("#add");
+	addButton.style.display = "block";
+	addButton.style.margin = "1em 6em";
+	addButton.addEventListener("click", ()=>{
+		let col1 = document.querySelector("#col1");
+		let col2 = document.querySelector("#col2");
+		let col3 = document.querySelector("#col3");
+
+		col1.innerText = name.value;
+		col2.innerText = phone.value;
+		col3.innerText = mail.value;
+	});
 }
+
