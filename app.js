395bf6e8a19b53441bf39c6764c5011dbad26117
Mr.AK <akhilsaivaraganti@gmail.com>
2021-03-03 09:54:05 +0530

    need to implement edit function

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ submitButton.addEventListener("click", () => {
 			<td>${phone.value}</td>
 			<td>${mail.value}</td>
       <button onclick = "deleteFunction(this)">delete</button>
-      <button onclick = "editFunction()">edit</button>
+      <button onclick = "editFunction(this)">edit</button>
 			`;
       table.appendChild(tablerow);
       clearInputs(name, phone, mail);
@@ -38,8 +38,16 @@ function clearInputs(name, phone, mail) {
 }
 
 function deleteFunction(tablerow) {
-  let r = tablerow.parentNode.rowIndex;
-  table.deleteRow(r);
+  let row = tablerow.parentNode.rowIndex;
+  table.deleteRow(row);
 }
 
-function editFunction() {}
+function editFunction(tablecol) {
+  let tableData = document.querySelector("td");
+  let name = document.querySelector(".name");
+  console.log(tableData);
+  let i = 0;
+  for (i = 0; i < tableData.length; i++) {
+    console.log(tableData);
+  }
+}
